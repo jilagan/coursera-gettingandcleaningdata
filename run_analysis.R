b858ecdab69fd43c8b50c5d71628769023b635a7
Joseph Ilagan <joben.ilagan@gmail.com>
2014-06-16 08:29:55 +0800

    Coded mean and standard column extraction procedure.

--- run_analysis.R
+++ run_analysis.R
@@ -16,6 +16,9 @@ print(data_dir)
 print(train_dir)
 print(test_dir)
 
+# Files
+features_file <- paste0(data_dir,"features.txt")
+
 # Clear variables in working space
 if (exists("X_train")) rm(X_train)
 if (exists("y_train")) rm(y_train)
@@ -26,6 +29,9 @@ if (exists("subject_test")) rm(subject_test)
 if (exists("X")) rm(X)
 if (exists("y")) rm(y)
 if (exists("subject")) rm(subject)
+if (exists("X_extracted")) rm(X_extracted)
+if (exists("y_extracted")) rm(y_extracted)
+
 
 # Merge the training and test sets to create one data set
 X_train <- read.table(paste0(train_dir,"X_train.txt"))
@@ -41,6 +47,12 @@ y <- rbind(y_train, y_test)
 subject <- rbind(subject_train, subject_test)
 
 # Extract mean and standard deviation of each measurement
+features <- read.table(features_file)
+# -- Columns to extract
+meaningfulFeatures <- features[grep("[mM]ean|[sS]td",features$V2),]
+meaningfulCols <- features$V1[grep("[mM]ean|[sS]td",features$V2)]
+X_extracted <- X[,meaningfulCols]
+y_extracted <- y[,meaningfulCols]
 
 # Add descriptive activity names
 
@@ -48,3 +60,10 @@ subject <- rbind(subject_train, subject_test)
 
 
 # Create tidy data set with the average of each activity and each subject
+# - merge subject info with X and y
+
+# - get average/mean
+
+# - write to file
+
+
